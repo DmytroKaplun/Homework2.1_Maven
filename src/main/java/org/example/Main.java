25a5f822a7252b830f3a30ed63b3032af635b324
DmytroKaplun <newtonknight1990@gmail.com>
2024-04-06 14:22:31 +0300

    fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import com.google.gson.Gson;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<String, String> name = new LinkedHashMap<>();
+        name.put("name", "Dmytro");
+        name.put("lastName", "Kaplun");
+        System.out.println(new Gson().toJson(name));
     }
+
 }
